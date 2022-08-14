1488f652cc8982c248bba723c2ad51825c805303
Vinicius Raupp <viniciusrauppcarlos@gmail.com>
2022-08-14 17:31:50 -0300

    aa

---
+++ Algoritmos e Programação/02_1108/exercicios/10/10.py
@@ -0,0 +1,6 @@
+#F.U.A que leia o preço de um produto e a quantidade comprada e exiba para o usuário
+#o preço que ele tem que pagar pela compra.
+
+preco = float(input("Digite o preço do produto comprado: R$"))
+quantid = float(input("Digite a quantidade comprada: "))
+

7c6d82c3568d333ed32e7eed294e15e5f34d1ffe
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2022-08-25 22:14:16 -0300

    aula dia 28

--- Algoritmos e Programação/04_2508/exercicios/01/01.py
+++ Algoritmos e Programação/04_2508/exercicios/01/01.py
@@ -7,16 +7,14 @@
 #--------Imprima na tela a frase O Número é menor que 10.ok
 #(4) se o número for maior que 10:
 #--------Imprima na tela a frase O Número é maior que 10.ok
-#(5) imprima na tela o quadrado de cada número:
-#(6) imprima na tela o resto da divisão de cada número por 2:
-#(7) imprima na tela a soma dos dois números;
+#(5) imprima na tela o quadrado de cada número: ok
+#(6) imprima na tela o resto da divisão de cada número por 2: ok
+#(7) imprima na tela a soma dos dois números; ok
 #Instrução: O Algoritmo deve primeiramente receber as duas entradas e após imprimir na tela as solicitações na ordem proposta.
 
-from traceback import print_tb
-#teste
 
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segunda número: "))
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segunda número: "))
 
 
 #par ou impar
@@ -39,3 +37,25 @@ if (num2>10):
     print("O segundo número é maior que 10.")
 else:
     print("O segundo número é menor que 10.")
+
+#quadrado do número
+
+num1quad = num1 * num1
+num2quad = num2 * num2
+
+print("O quadrado do primeiro número é: ",num1quad)
+print("O quadrado do segundo número é: ",num2quad)
+
+#resto da divisão por 2
+
+num1restdiv = num1 % 2
+num2restdiv = num2 % 2
+
+print("O resto da divisão do primeiro número por 2 é: ",num1restdiv)
+print("O resto da divisão do segundo número por 2 é: ",num2restdiv)
+
+#soma dos dois números
+
+soma = num1+num2
+
+print("A soma entre os dois números é: ",soma)
---
+++ Algoritmos e Programação/04_2508/exercicios/02/01.py
@@ -0,0 +1,5 @@
+x = 0
+while x < 50:
+    if x%2!= 0:
+        print(x)
+    x = x + 1 

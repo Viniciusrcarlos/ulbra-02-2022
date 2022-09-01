7d782b93d6cbec16aa27f7649b56805b3f3decf2
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2022-08-31 21:42:45 -0300

    table

---
+++ Algoritmos e Programação/05_0109/exercicios/01/01.py
@@ -0,0 +1,5 @@
+#Envio da Resposta do TDE - *Faça um programa que leia um valor inteiro (que representa o real, moeda nacional) e calcule
+#qual o menor número possível de notas/moedas de 100, 50, 20, 10, 5, 2 e 1 em que o valor lido
+#pode ser decomposto. Escrever o valor lido e a relação de notas necessárias.*
+
+

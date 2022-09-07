153a490671c9aacc2f17286b1d50635998700fae
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2022-09-07 17:13:40 -0300

    py

---
+++ Algoritmos e Programação/06_0809/exercicios/01/01.py
@@ -0,0 +1,15 @@
+#Dados dois números A e B, some 100 ao maior número e imprima
+
+num1 = int(input("Digite o número A: "))
+num2 = int(input("Digite o número B: "))
+
+if num1 > num2:
+    soma=num1+100
+    print(soma)
+elif num2 > num1:
+    soma=num2+100
+    print(soma)
+    
+
+
+
